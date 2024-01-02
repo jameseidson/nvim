4eebf101383d26e72e597b895c27bb8fbe7716d4
jameseidson <eidsonje@icloud.com>
2024-01-01 18:43:43 -0700

    fix clangd running on proto files

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters = {
+      clang_format = {
+        inherit = true,
+        prepend_args = { "--fallback-style=google" },
+      },
+    },
+    formatters_by_ft = {
+      ["cpp"] = { "clang_format" },
+      ["javascript"] = { "prettierd" },
+      ["javascriptreact"] = { "prettierd" },
+      ["typescript"] = { "prettierd" },
+      ["typescriptreact"] = { "prettierd" },
+      ["vue"] = { "prettierd" },
+      ["css"] = { "prettierd" },
+      ["scss"] = { "prettierd" },
+      ["less"] = { "prettierd" },
+      ["html"] = { "prettierd" },
+      ["json"] = { "prettierd" },
+      ["jsonc"] = { "prettierd" },
+      ["yaml"] = { "prettierd" },
+      ["markdown"] = { "prettierd" },
+      ["markdown.mdx"] = { "prettierd" },
+      ["graphql"] = { "prettierd" },
+      ["handlebars"] = { "prettierd" },
+    },
+  },
+}
